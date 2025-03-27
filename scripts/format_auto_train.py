84dfb4e891060cd9bf396e8d33150923373a0a68
anhalu <hoang.minh.an@sun-asterisk.com>
2025-03-27 08:29:14 -0700

    update category

--- scripts/format_auto_train.py
+++ scripts/format_auto_train.py
@@ -7,6 +7,6 @@ from cores.distillation.format_dataset import format_autotrain_sft
 
 
 format_autotrain_sft(
-    'data_baseline_v4/baseline_v5.csv', 
-    'data_baseline_v4/baseline_v5_autotrain.csv'
+    'data_baseline_v4/baseline_v6.csv', 
+    'data_baseline_v4/baseline_v6_autotrain.csv'
 )
