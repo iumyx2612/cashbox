5d5dce31cc5a8960a5bf5db4b9d9dd188880a9e1
anhalu <hoang.minh.an@sun-asterisk.com>
2025-06-20 23:25:46 -0700

    Fix conflict

--- scripts/test.py
+++ scripts/test.py
@@ -1,21 +1,8 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import pandas as pd 
 
 
-model_id = 'Qwen/Qwen3-8B'
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-messages = [
-    {
-        "role": "system", "tool_name": "calculator", "content": "56088"
-    },
-    {
-        "role": "tool",
-        "tool_name": "weather_api",
-        "content": "{'condition': 'rain', 'temperature': 15}",
-    },
-]
-a = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False
-)
+path = 'data/time_reasoning/function_calling_v3.csv'
+df = pd.read_csv(path)
 
-print(a)
+
+print(df.iloc[1000]['system'])
