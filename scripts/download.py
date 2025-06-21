4ff1a2453a7eb79a02d6b5ee247df2fd58c5bdf6
anhalu <anhalu.dev@gmail.com>
2025-06-21 07:02:31 +0000

    Fix production

--- scripts/download.py
+++ scripts/download.py
@@ -1,48 +1,19 @@
 from huggingface_hub import snapshot_download
+import os
 
-snapshot_download("unsloth/Qwen3-14B-unsloth-bnb-4bit")
+# Thiết lập token Hugging Face (phải có quyền truy cập model private)
+HF_TOKEN = os.getenv("HUGGINGFACE__TOKEN")
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# ID model trên Hugging Face (ví dụ: 'anhalu/qwen-baseline-time-function-calling-v3')
+model_id = "anhalu/qwen-baseline-time-function-calling-v3"
 
-model_name = "unsloth/Qwen3-14B-unsloth-bnb-4bit"
-
-# load the tokenizer and the model
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype="auto",
-    device_map="auto"
+# Tải model
+snapshot_download(
+    repo_id=model_id,
+    repo_type="model",
+    use_auth_token=HF_TOKEN,   # Cần dòng này nếu model là private
+    local_files_only=False,    # Đảm bảo sẽ tải từ Hugging Face về
+    resume_download=True       # Cho phép tiếp tục nếu trước đó bị ngắt
 )
 
-# prepare the model input
-prompt = "Give me a short introduction to large language model."
-messages = [
-    {"role": "user", "content": prompt}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-# conduct text completion
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=32768
-)
-output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
-
-# parsing thinking content
-try:
-    # rindex finding 151668 (</think>)
-    index = len(output_ids) - output_ids[::-1].index(151668)
-except ValueError:
-    index = 0
-
-thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
-content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
-
-print("thinking content:", thinking_content)
-print("content:", content)
+print(f"✅ Model downloaded")
